c8c5a6a55b7e1d7683d667cca50a324dce50a320
michal <michal.blaszczyk1997@gmail.com>
2020-05-10 19:15:36 +0200

    Create back-end online shop project structure

---
+++ src/main/java/com/example/BoardGamesOnlineShop/BoardGamesOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.BoardGamesOnlineShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGamesOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGamesOnlineShopApplication.class, args);
+	}
+
+}
